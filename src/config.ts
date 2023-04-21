7c90673702d6097fa60777d484c48519068d9dd3
Johnatan R <johnatan.r1000@gmail.com>
2023-04-20 19:41:34 -0500

    Se establece la conexión con la BD.

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('configuration', () => {
+  return {
+    database: {
+      // Hace referencia a los archivos '.env'
+      username: process.env.MONGO_USERNAME,
+      password: process.env.MONGO_PASSWORD,
+      db: process.env.MONGO_DB,
+      cluster: process.env.MONGO_CLUSTER
+    },
+    email: {
+      username: process.env.EMAIL,
+      password: process.env.EMAIL_PASSWORD,
+    },
+  };
+});
