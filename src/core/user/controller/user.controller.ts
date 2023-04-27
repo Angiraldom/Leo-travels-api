b2fc163817338a73da0be292f295472c584eda8c
Carlos Lopez <carloseli0297@gmail.com>
2023-04-27 16:51:55 -0500

    Añadiendo mejora en los mensajes de respuesta del aplicativo.

--- src/core/user/controller/user.controller.ts
+++ src/core/user/controller/user.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+
+import { Response } from 'express';
 
 import { UserService } from '../service/user.service';
 import { AddUserDto, DeleteUserDto, UpdateUserDto } from '../dto';
-import { IRequestResponse } from '../../../shared/utils/interface/IRequestResponse.interface';
 
 @Controller('user')
 export class UserController {
@@ -12,35 +13,55 @@ export class UserController {
    * Controller of the method getAllUsers.
    */
   @Get('all')
-  async getAllUsers(): Promise<IRequestResponse> {
-    return await this.userService.getAllUsers();
+  async getAllUsers(@Res() res: Response) {
+    try {
+      const RESPONSE = await this.userService.getAllUsers();
+      return res.status(RESPONSE['code']).json(RESPONSE);
+    } catch (error) {
+      console.log('Error método: getAllUsers');
+      return res.status(400).json(error);
+    }
   }
 
   /**
    * Controller of the method addUser.
    */
   @Post('add')
-  async addUser(@Body() user: AddUserDto): Promise<IRequestResponse> {
-    return await this.userService.addUser(user);
+  async addUser(@Body() user: AddUserDto, @Res() res: Response) {
+    try {
+      const RESPONSE = await this.userService.addUser(user);
+      return res.status(RESPONSE['code']).json(RESPONSE);
+    } catch (error) {
+      console.log('Error método: addUser');
+      return res.status(400).json(error);
+    }
   }
 
   /**
    * Controller of the method updateUser.
    */
   @Post('update')
-  async updateUser(
-    @Body() userToUpdate: UpdateUserDto,
-  ): Promise<IRequestResponse> {
-    return await this.userService.updateUser(userToUpdate);
+  async updateUser(@Body() userToUpdate: UpdateUserDto, @Res() res: Response) {
+    try {
+      const RESPONSE = await this.userService.updateUser(userToUpdate);
+      return res.status(RESPONSE['code']).json(RESPONSE);
+    } catch (error) {
+      console.log('Error método: updateUser');
+      return res.status(400).json(error);
+    }
   }
 
   /**
    * Controller of the method deleteUSer.
    */
   @Post('delete')
-  async deleteUSer(
-    @Body() userToDelete: DeleteUserDto,
-  ): Promise<IRequestResponse> {
-    return await this.userService.deleteUser(userToDelete);
+  async deleteUSer(@Body() userToDelete: DeleteUserDto, @Res() res: Response) {
+    try {
+      const RESPONSE = await this.userService.deleteUser(userToDelete);
+      return res.status(RESPONSE['code']).json(RESPONSE);
+    } catch (error) {
+      console.log('Error método: updateUser');
+      return res.status(400).json(error);
+    }
   }
 }
